76c8a6308abc9092dfc23356a0d7b676bf363ca4
unknown <leducminh22003@gmail.com>
2023-03-07 15:59:43 +0700

    nop

---
+++ ss3_Angular_Overview/calculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,35 @@
+// @ts-ignore
+import {Component, OnInit} from '@angular/core';
+
+// @ts-ignore
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  result = undefined;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  sum(firstNumber: string, secondNumber: string) {
+    debugger
+    this.result = parseInt(firstNumber, 10) + parseInt(secondNumber, 10);
+  }
+
+  minus(firstNumber: string, secondNumber: string) {
+    this.result=parseInt(firstNumber,10)-parseInt(secondNumber,10);
+  }
+
+  double(firstNumber: string, secondNumber: string) {
+    this.result=parseInt(firstNumber,10)*parseInt(secondNumber,10);
+  }
+
+  division(firstNumber: string, secondNumber: string) {
+    this.result=parseInt(firstNumber,10)/parseInt(secondNumber,10);
+  }
+}
