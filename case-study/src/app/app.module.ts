84602b8bc358137594b1094b7342e1fb00bf73f1
unknown <leducminh22003@gmail.com>
2023-03-08 14:01:18 +0700

    update bt

--- case-study/src/app/app.module.ts
+++ case-study/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { FacilityListComponent } from './component/facility/facility-list/facility-list.component';
 import { FacilityEditComponent } from './component/facility/facility-edit/facility-edit.component';
 import { FacilityCreateComponent } from './component/facility/facility-create/facility-create.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FacilityListComponent,
     FacilityEditComponent,
-    FacilityCreateComponent
+    FacilityCreateComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
